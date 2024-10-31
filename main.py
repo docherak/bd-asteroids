180de299f43be5fadd05e9b9d8dda4edf43dc3e6
docherak <docherak@protonmail.ch>
2024-10-31 23:58:50 +0100

    feat: implement group management for update and draw operations

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
@@ -18,9 +23,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+
+        for u in updatable:
+            u.update(dt)
+
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
