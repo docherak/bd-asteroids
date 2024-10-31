ed7dd767baf892f1e2dbc53a7500dfa56b9ab8ef
docherak <docherak@protonmail.ch>
2024-10-31 23:07:21 +0100

    feat: add fps cap and delta time tracking

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from constants import *
 
 def main():
     print("Starting asteroids!")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +16,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
